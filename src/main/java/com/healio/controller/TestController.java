18512eeb9296645d4699e85a034da368804bbd17
SUJITKUMARVIPUL <sujitkumarvipul@gmail.com>
2026-06-14 04:02:32 +0530

    chore: clear cloud credentials and update pipeline

--- src/main/java/com/healio/controller/TestController.java
+++ src/main/java/com/healio/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.healio.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test")
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping
     public String test(){
         System.out.println("test");
+        log.info(">>>> TEST ENDPOINT WAS HIT SUCCESFULLY! <<<<");
         return "test";
     }
 
